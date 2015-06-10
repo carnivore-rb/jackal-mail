da8a363f7323b2e34511f924e021142dd696e46e
Chris Roberts <code@chrisroberts.org>
2015-06-10 06:14:43 -0700

    Add service registration

--- lib/jackal-mail.rb
+++ lib/jackal-mail.rb
@@ -6,3 +6,19 @@ module Jackal
     autoload :Smtp, 'jackal-mail/smtp'
   end
 end
+
+Jackal.service(
+  :mail,
+  :description => 'Send mail notification',
+  :category => :notifier,
+  :configuration => {
+    :via_options => {
+      :type => :hash,
+      :description => 'SMTP configuration options'
+    },
+    :bcc => {
+      :type => :string,
+      :description => 'BCC address for all mail'
+    }
+  }
+)
